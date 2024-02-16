26696cfab820d7e6f561bd808f68e90f0255f367
Casey DeBuse <75276126+cjdebuse@users.noreply.github.com>
2024-02-16 18:05:40 -0500

    Add pixel density change functionality

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 
 const body = document.querySelector('body');
-const displayArea = document.createElement('div');
-displayArea.className = 'display-area';
+const button = document.querySelector('button');
+const displayArea = document.querySelector('.display-area');
 
-for (let i = 1; i <= 64; i++) {
-    let pixelRow = document.createElement('div');
-    pixelRow.className = 'pixel-row';
-    for (let j = 1; j <= 64; j++) {
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        pixelRow.appendChild(pixel);
+createCanvas(64);
+
+button.addEventListener('click', () => {
+    clearDisplayArea();
+    createCanvas(getPixelDensity());
+});
+
+function getPixelDensity () {
+    const numberOfPixels = Number(prompt("Enter the desired number of horizontal pixels (≤ 100):", 64));
+    if (numberOfPixels && numberOfPixels <= 100) {
+        return numberOfPixels;
+    } else {
+        return(getPixelDensity());
     }
-    displayArea.appendChild(pixelRow);
 }
 
-body.appendChild(displayArea);
+function clearDisplayArea () {
+    const pixelRows = document.querySelectorAll('.pixel-row');
+    pixelRows.forEach((row) => {
+        displayArea.removeChild(row);
+    });
+}
+
+function createCanvas (numberOfPixels) {
+    for (let i = 1; i <= numberOfPixels; i++) {
+        let pixelRow = document.createElement('div');
+        pixelRow.className = 'pixel-row';
+        for (let j = 1; j <= numberOfPixels; j++) {
+            let pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            pixelRow.appendChild(pixel);
+        }
+        displayArea.appendChild(pixelRow);
+    }
+}
 
 displayArea.addEventListener('mouseover', changePixel);
 
